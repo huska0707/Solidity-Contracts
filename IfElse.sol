bae1215db78cf373a7283d013329e20209b9d121
huska0707 <hussaink001haled@gmail.com>
2024-06-24 15:22:29 -0500

    For and While Loop

--- IfElse.sol
+++ IfElse.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.24;
 
 contract IfElse {
-    function foo(uint256 x) public pure returns(uint256) {
+    function foo(uint256 x) public pure returns (uint256) {
         if (x < 10) {
             return 0;
         } else if (x < 20) {
@@ -12,7 +12,26 @@ contract IfElse {
         }
     }
 
-    function ferteny(uint256 _x) public pure returns(uint256) {
+    function ferteny(uint256 _x) public pure returns (uint256) {
         return _x < 10 ? 1 : 2;
+    }
+
+    function loop() public {
+        for (uint256 i = 0; i < 10; i++) {
+            // skip to the  next iteration with continue
+            if (i == 3) {
+                continue;
+            }
+            // Exit loop with break
+            if (i == 7) {
+                break;
+            }
+        }
+
+        uint256 j;
+
+        while (j < 10) {
+            j++;
+        }
     }
 }
