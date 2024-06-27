ca32338ea2410699508c5f054f0ceeefe8419626
huska0707 <hussaink001haled@gmail.com>
2024-06-26 21:51:50 -0500

    ReentrancyGuardTransient and ReentrancyGuard

--- TransientStorage.sol
+++ TransientStorage.sol
@@ -35,4 +35,63 @@ contract TestStorage {
     }
 }
 
-contract TransientStorage {}
+contract TestTransientStorage {
+    bytes32 constant SLOT = 0;
+
+    function test() public {
+        assembly {
+            tstore(SLOT, 321)
+        }
+
+        bytes memory b = "";
+        msg.sender.call(b);
+    }
+
+    function val() public view returns (uint256 v) {
+        assembly {
+            v := tload(SLOT)
+        }
+    }
+}
+
+contract ReentrancyGuard {
+    bool private locked;
+
+    modifier lock() {
+        require(!locked);
+        locked = true;
+        _;
+        locked = false;
+    }
+
+    // 35313 gas
+    function test() public lock {
+        // Ignore call error
+        bytes memory b = "";
+        msg.sender.call(b);
+    }
+}
+
+contract ReentrancyGuardTransient {
+    bytes32 constant SLOT = 0;
+
+    modifier lock() {
+        assembly {
+            if tload(SLOT) {
+                revert(0, 0)
+            }
+            tstore(SLOT, 1)
+        }
+        _;
+        assembly {
+            tstore(SLOT, 0)
+        }
+    }
+
+    // 21887 gas
+    function test() external lock {
+        // Ignore call error
+        bytes memory b = "";
+        msg.sender.call(b);
+    }
+}
